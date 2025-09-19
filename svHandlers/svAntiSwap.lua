1f0d979e1049954997ff90f43eb1ee81f2528ef0
Forcng <164341281+Forcng@users.noreply.github.com>
2025-09-19 17:04:23 -0400

    Refactor svAntiSwap.lua for cleaner code

--- svHandlers/svAntiSwap.lua
+++ svHandlers/svAntiSwap.lua
@@ -11,21 +11,14 @@ exports.ox_inventory:registerHook('swapItems', function(data)
     local action = data.action
     local itemName = (data.fromSlot and data.fromSlot.name) or (data.toSlot and data.toSlot.name)
 
-    if not itemName then
-        return
-    end
+    if not itemName then return end
 
-    if (action == "take" or action == "give") and blockedItems[itemName] then
-        return false
-    end
+    if (action == "take" or action == "give") and blockedItems[itemName] then return false end
 
-    if action == "move" and data.toType == "drop" and blockedItems[itemName] then
-        return false
-    end
+    if action == "move" and data.toType == "drop" and blockedItems[itemName] then return false end
 
-    if action == "move" and data.toType == "glovebox" and blockedItems[itemName] then
-        return false
-    end
+    if action == "move" and data.toType == "glovebox" and blockedItems[itemName] then return false end
 end, {
     print = false
 })
+
